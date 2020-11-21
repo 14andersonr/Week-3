b95f43a12b05bd027b716310785669b6483fd803
Ricky <14andersonr@gmail.com>
2020-11-21 15:44:25 -0500

    Initial Commit

---
+++ RJA3 Week3 Challenges/Games.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RJA3_Week3_Challenges
+{
+    public abstract class Games
+    {
+        public string Title { get; set; }
+
+        public int GameRating { get; set; }
+
+        public bool FitforKids { get; set; }
+
+        public virtual void GetFeedback() // virtual classes can have a body, abstract cannot.
+        {
+            if (Title == null)
+            {
+                Console.WriteLine("What's the Title of the game?");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("");
+            }
+        }
+
+        //public abstract void Testing(string test);
+
+    }
+}
